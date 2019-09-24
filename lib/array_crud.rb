49eac3b71696b247c353a255f66879e3a04ec6a5
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-09-24 00:52:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array 
-  ["I", "am", "Really", "Learning"]
+  ["I", "am", "really", "Learning"]
 end
 
 def add_element_to_end_of_array(array, element)
